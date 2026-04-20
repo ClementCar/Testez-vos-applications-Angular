44471e343beebb08f25692d407b8a5f760786d58
ClementCar <clementcarcenac@gmail.com>
2026-04-20 17:24:37 +0200

    files

---
+++ src/app/features/table-management/services/table-display.service.spec.ts
@@ -0,0 +1,8 @@
+import { TableDisplayService } from "./table-display.service";
+
+describe('tableDisplayService', () => {
+
+    it('should return "Available"', () => {
+        const service = new TableDisplayService();
+    })
+});
