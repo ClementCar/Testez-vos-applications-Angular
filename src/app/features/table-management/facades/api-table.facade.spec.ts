18b3b8a70df61b86a139ec57b7788ee2e7742bca
ClementCar <clementcarcenac@gmail.com>
2026-04-22 17:42:35 +0200

    Test api table

---
+++ src/app/features/table-management/facades/api-table.facade.spec.ts
@@ -0,0 +1,49 @@
+import { TestBed } from "@angular/core/testing";
+import { ApiTableFacade } from "./api-table.facade"
+import { provideZonelessChangeDetection } from "@angular/core";
+import { TableService } from "../services/table.service";
+import { ApiTableService } from "../services/api-table.service";
+import { provideHttpClient } from "@angular/common/http";
+import { HttpTestingController, provideHttpClientTesting } from "@angular/common/http/testing";
+import { TEST_TABLES } from "../../../test-data/test-tables";
+
+describe('ApiTableFacade', () => {
+    let facade: ApiTableFacade;
+    let httpCtrl: HttpTestingController
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [
+                ApiTableFacade, 
+                provideZonelessChangeDetection(),
+                { provide: TableService, useClass: ApiTableService },
+                provideHttpClient(),
+                provideHttpClientTesting()
+            ]
+        })
+
+        facade = TestBed.inject(ApiTableFacade);
+        httpCtrl = TestBed.inject(HttpTestingController);
+    })
+
+    it('should be created', () => {
+        expect(facade).toBeTruthy();
+    })
+
+    it('should load all tables if they are not already loaded', () => {
+        const tables = facade.allTables();
+        const req = httpCtrl.expectOne('/api/tables');
+        req.flush(TEST_TABLES);
+
+        expect(tables()).toEqual(TEST_TABLES);
+    })
+
+    // TODO
+    // it('availables tables', () => {
+    //     const availables = facade.availableTablesForPartySize();
+    // })
+
+    afterEach(() => {
+        httpCtrl.verify();
+    })
+})
