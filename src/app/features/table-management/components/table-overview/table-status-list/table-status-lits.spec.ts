da1a691abebc402ce4ec496f35feb2ba70e2ca83
ClementCar <clementcarcenac@gmail.com>
2026-04-23 14:55:13 +0200

    end test

---
+++ src/app/features/table-management/components/table-overview/table-status-list/table-status-lits.spec.ts
@@ -0,0 +1,108 @@
+import { TestBed } from '@angular/core/testing';
+import { provideZonelessChangeDetection } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
+import { ApiTableFacade } from '../../../facades/api-table.facade';
+import { ApiTableService } from '../../../services/api-table.service';
+import { TableService } from '../../../services/table.service';
+import { TEST_TABLES } from '../../../../../test-data/test-tables';
+import { Table } from '../../../models/table.model';
+
+describe('ApiTableFacade', () => {
+  let facade: ApiTableFacade;
+  let httpCtrl: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        ApiTableFacade,
+        provideZonelessChangeDetection(),
+        { provide: TableService, useClass: ApiTableService },
+        provideHttpClient(),
+        provideHttpClientTesting()
+      ]
+    });
+
+    facade = TestBed.inject(ApiTableFacade);
+    httpCtrl = TestBed.inject(HttpTestingController);
+  });
+
+  it('should be created', () => {
+    expect(facade).toBeTruthy();
+  });
+
+  it('should load all tables if they are not already loaded', () => {
+    const tables = facade.allTables();
+    const req = httpCtrl.expectOne('/api/tables');
+    req.flush(TEST_TABLES);
+
+    expect(tables()).toEqual(TEST_TABLES);
+  });
+
+  describe('Available tables', () => {
+
+    it('should show no tables available for a negative party size', () => {
+      const tables = facade.availableTablesForPartySize(-1);
+      expect(tables()).toEqual([]);
+    });
+
+    it('should show no tables available for a party size of 0', () => {
+      const tables = facade.availableTablesForPartySize(0);
+      expect(tables()).toEqual([]);
+    });
+
+    it('should show all available tables with sufficient capacity', () => {
+      const partySize = 5;
+      const tables = facade.availableTablesForPartySize(partySize);
+      const req = httpCtrl.expectOne('/api/tables');
+      req.flush(TEST_TABLES);
+      const expected = TEST_TABLES.filter(table => table.capacity >= partySize && table.status === 'available');
+      expect(tables()).toEqual(expected);
+    });
+
+  });
+
+  describe('Seat party at table', () => {
+
+    it('should update the table on the server', () => {
+      const tableId = 'table1';
+      const partySize = 5;
+      facade.seatPartyAtTable(tableId, partySize).subscribe();
+      const req = httpCtrl.expectOne(`/api/tables/${ tableId }/seat`);
+      expect(req.request.body).toEqual({ status: 'occupied', partySize});
+    });
+
+    it('should mark the table as occupied', () => {
+      const table: Table = { id: 'table1', status: 'available', capacity: 6, number: 1 };
+      const tables = facade.allTables();
+      const req = httpCtrl.expectOne(`/api/tables`);
+      req.flush([table]);
+      facade.seatPartyAtTable(table.id, table.capacity).subscribe();
+      const occupiedTable: Table = { ...table, status: 'occupied' };
+      httpCtrl.expectOne(`/api/tables/${ table.id }/seat`).flush(occupiedTable);
+      expect(tables()).toEqual([occupiedTable]);
+    });
+
+    it('should mark the table as occupied (REFACTORED)', () => {
+      const table: Table = { id: 'table1', status: 'available', capacity: 6, number: 1 };
+      const tables = allTablesFlushedWith(table);
+      facade.seatPartyAtTable(table.id, table.capacity).subscribe();
+      const occupiedTable: Table = { ...table, status: 'occupied' };
+      httpCtrl.expectOne(`/api/tables/${ table.id }/seat`).flush(occupiedTable);
+      expect(tables()).toEqual([occupiedTable]);
+    });
+
+    function allTablesFlushedWith(table: Table) {
+      const tables = facade.allTables();
+      const req = httpCtrl.expectOne(`/api/tables`);
+      req.flush([table]);
+      return tables;
+    }
+
+  });
+
+  afterEach(() => {
+    httpCtrl.verify();
+  });
+
+});
--- src/app/features/table-management/facades/api-table.facade.spec.ts
+++ src/app/features/table-management/facades/api-table.facade.spec.ts
@@ -6,6 +6,7 @@ import { ApiTableService } from "../services/api-table.service";
 import { provideHttpClient } from "@angular/common/http";
 import { HttpTestingController, provideHttpClientTesting } from "@angular/common/http/testing";
 import { TEST_TABLES } from "../../../test-data/test-tables";
+import { Table } from "../models/table.model";
 
 describe('ApiTableFacade', () => {
     let facade: ApiTableFacade;
@@ -38,10 +39,54 @@ describe('ApiTableFacade', () => {
         expect(tables()).toEqual(TEST_TABLES);
     })
 
-    // TODO
-    // it('availables tables', () => {
-    //     const availables = facade.availableTablesForPartySize();
-    // })
+    describe('Available tables', () => {
+        
+        it('should show no tables available for a negative party size', () => {
+            const tables = facade.availableTablesForPartySize(-1);
+            expect(tables()).toEqual([]);
+        });
+
+        it('should show no tables available for a party size of 0', () => {
+            const tables = facade.availableTablesForPartySize(0);
+            expect(tables()).toEqual([]);
+        });
+
+        it('should show all available tables with sufficient capacity', () => {
+            const partySize = 5;
+            const tables = facade.availableTablesForPartySize(partySize);
+            const req = httpCtrl.expectOne('/api/tables');
+            req.flush(TEST_TABLES);
+            const expected = TEST_TABLES.filter(table => table.capacity >= partySize && table.status === 'available');
+            expect(tables()).toEqual(expected);
+        });
+    })
+
+    describe('Seat party at table', () => {
+
+        it('should update the table on the server', () => {
+            const tableId = 'table1';
+            const partySize = 5;
+            facade.seatPartyAtTable(tableId, partySize).subscribe();
+            const req = httpCtrl.expectOne(`/api/tables/${tableId}/seat`);
+            expect(req.request.body).toEqual({status: 'occupied', partySize: 5});
+        })
+
+        it('should mark the table as occupied (REFACTORED)', () => {
+            const table: Table = { id: 'table1', status: 'available', capacity: 6, number: 1 };
+            const tables = allTablesFushedWith(table);
+            facade.seatPartyAtTable(table.id, table.capacity).subscribe();
+            const occupiedTable: Table = { ...table, status: 'occupied' };
+            httpCtrl.expectOne(`/api/tables/${ table.id }/seat`).flush(occupiedTable);
+            expect(tables()).toEqual([occupiedTable]);
+        });
+
+        function allTablesFushedWith(table: Table){
+            const tables = facade.allTables();
+            const req = httpCtrl.expectOne('/api/tables');
+            req.flush([table]);
+            return tables;
+        }
+    })
 
     afterEach(() => {
         httpCtrl.verify();
